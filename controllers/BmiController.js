aa7dd1d543876bc6f384a1d70affd150dd3e7695
Mayank Raj <mayankrj1224@gmail.com>
2023-04-12 00:21:20 +0530

    .

--- controllers/BmiController.js
+++ controllers/BmiController.js
@@ -2,8 +2,7 @@ const BmiModel = require('../models/BmiModel');
 
 const bmiCalculation = async (req, res) => {
     try {
-        const { height, weight } = req.body;
-        const user = req.userid;
+        const { height, weight ,user} = req.body;
         const heightInMeter = (height * 0.305)
         const bmi = weight / Math.pow(heightInMeter, 2);
 
